f46729711dfaa5f6589c1cb964f92302168d8406
Thepropollo <ramonb634@gmail.com>
2026-01-04 15:41:32 -0500

    refactor: Migrate shadow styles to `boxShadow` and add package overrides.

--- src/core/constants/theme.js
+++ src/core/constants/theme.js
@@ -78,24 +78,15 @@ export const BORDER_RADIUS = {
 // Sombras
 export const SHADOWS = {
   SMALL: {
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.18,
-    shadowRadius: 1.0,
+    boxShadow: '0px 1px 1px rgba(0,0,0,0.18)',
     elevation: 1,
   },
   MEDIUM: {
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
+    boxShadow: '0px 2px 2.22px rgba(0,0,0,0.22)',
     elevation: 3,
   },
   LARGE: {
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.30,
-    shadowRadius: 4.65,
+    boxShadow: '0px 4px 4.65px rgba(0,0,0,0.30)',
     elevation: 8,
   },
 };
